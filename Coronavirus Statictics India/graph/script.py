7b149122e16472604e407c9a65299960b7bf0a2a
ShrayankM <shraymist@gmail.com>
2021-09-08 16:58:10 +0530

    updated main script for data change

--- Coronavirus Statictics India/graph/script.py
+++ Coronavirus Statictics India/graph/script.py
@@ -19,15 +19,21 @@ import os
 import warnings
 warnings.filterwarnings("ignore")
 
-url = "https://api.covid19india.org/states_daily.json"
+# url = "https://api.covid19india.org/csv/latest/state_wise_daily.csv"
+# url = "https://api.covid19india.org/states_daily.json"
+url = "https://data.covid19india.org/csv/latest/state_wise_daily.csv";
 # from graph import models
 
 def make_data():
-    with open('data.json') as f:
-        data = json.load(f)
-    data = data['states_daily']
-    df = pd.json_normalize(data)
-    return df
+    data = pd.read_csv('state_wise_daily.csv')
+    return data
+
+# def make_data():
+#     with open('data.json') as f:
+#         data = json.load(f)
+#     data = data['states_daily']
+#     df = pd.json_normalize(data)
+#     return df
 
 # def create_pie_chart(df, dir):
 #     df_pie = df[['status', 'tt']]
@@ -103,18 +109,18 @@ def create_pie_chart(df, dir, tdir):
 
 def create_line_chart(df, dir, tdir):
     df_ = df.copy(deep = True)
-    df_['date'] = pd.to_datetime(df_['date'])
-    df_ = df_[['date', 'status', 'tt']]
-    df_['Cases'] = pd.to_numeric(df_['tt'])
-    df_conf = df_[df_['status'] == 'Confirmed']
-    df_rec  = df_[df_['status'] == 'Recovered']
-    df_dec  = df_[df_['status'] == 'Deceased']
+    df_['Date'] = pd.to_datetime(df_['Date'])
+    df_ = df_[['Date', 'Status', 'TT']]
+    df_['Cases'] = pd.to_numeric(df_['TT'])
+    df_conf = df_[df_['Status'] == 'Confirmed']
+    df_rec  = df_[df_['Status'] == 'Recovered']
+    df_dec  = df_[df_['Status'] == 'Deceased']
     colors = []
     colors.append(px.colors.qualitative.G10[2])
     colors.append(px.colors.qualitative.G10[1])
     colors.append(px.colors.qualitative.G10[3])
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x= df_conf['date'], y = df_conf['tt'],
+    fig.add_trace(go.Scatter(x= df_conf['Date'], y = df_conf['TT'],
                              mode='markers',
                              marker=dict(
                              color='#F69A30',
@@ -123,7 +129,7 @@ def create_line_chart(df, dir, tdir):
                                 width=0.2
                              )),
                              name="Confirmed", line_shape='spline',))
-    fig.add_trace(go.Scatter(x= df_rec['date'], y = df_rec['tt'],
+    fig.add_trace(go.Scatter(x= df_rec['Date'], y = df_rec['TT'],
                              mode='markers',
                              marker=dict(
                              color='#387938',
@@ -132,7 +138,7 @@ def create_line_chart(df, dir, tdir):
                                 width=0.2
                              )),
                              name="Recovered", line_shape='spline'))
-    fig.add_trace(go.Scatter(x= df_dec['date'], y = df_dec['tt'],
+    fig.add_trace(go.Scatter(x= df_dec['Date'], y = df_dec['TT'],
                              mode='markers',
                              marker=dict(
                              color='#B2322B',
@@ -169,48 +175,48 @@ def create_line_chart(df, dir, tdir):
 
 def create_dataset(df):
     df_bar = df.tail(3)
-    df_bar.set_index('status', inplace = True)
-    df_bar.drop('date', axis = 1, inplace = True)
-    df_bar.drop('tt', axis = 1, inplace = True)
-    df_bar.drop('un', axis = 1, inplace = True)
-    df_bar.drop('dateymd', axis = 1, inplace = True)
+    df_bar.set_index('Status', inplace = True)
+    df_bar.drop('Date', axis = 1, inplace = True)
+    df_bar.drop('TT', axis = 1, inplace = True)
+    df_bar.drop('UN', axis = 1, inplace = True)
+    df_bar.drop('Date_YMD', axis = 1, inplace = True)
     df_bar = df_bar.apply(pd.to_numeric)
     df_bar = df_bar.T
     return df_bar
 
 def overall_info(df):
     df_states = df.copy(deep = True)
-    df_states['date'] = pd.to_datetime(df_states['date'])
-    df_states.drop('tt', axis = 1, inplace = True)
-    df_states.drop('dateymd', axis = 1, inplace = True)
+    df_states['Date'] = pd.to_datetime(df_states['Date'])
+    df_states.drop('TT', axis = 1, inplace = True)
+    df_states.drop('Date_YMD', axis = 1, inplace = True)
     ########## ALL States Confirmed Data #############################
-    df_C = df_states[df_states['status'] == 'Confirmed'].copy(deep = True)
-    df_C.drop('status', axis = 1, inplace = True)
-    df_MC = pd.melt(df_C, id_vars = 'date', value_vars = list(df_C.columns).remove('date'),
-                var_name = 'state', value_name = 'Confirmed')
+    df_C = df_states[df_states['Status'] == 'Confirmed'].copy(deep = True)
+    df_C.drop('Status', axis = 1, inplace = True)
+    df_MC = pd.melt(df_C, id_vars = 'Date', value_vars = list(df_C.columns).remove('Date'),
+                var_name = 'State', value_name = 'Confirmed')
     df_MC['Confirmed'] = pd.to_numeric(df_MC['Confirmed'])
-    df_MC.drop('date', axis = 1, inplace = True)
-    df_MC = df_MC.groupby('state').sum()
+    df_MC.drop('Date', axis = 1, inplace = True)
+    df_MC = df_MC.groupby('State').sum()
     df_MC = df_MC['Confirmed']
     ##################################################################
     ########## ALL States Recovered Data #############################
-    df_R = df_states[df_states['status'] == 'Recovered'].copy(deep = True)
-    df_R.drop('status', axis = 1, inplace = True)
-    df_MR = pd.melt(df_R, id_vars = 'date', value_vars = list(df_R.columns).remove('date'),
-                var_name = 'state', value_name = 'Recovered')
+    df_R = df_states[df_states['Status'] == 'Recovered'].copy(deep = True)
+    df_R.drop('Status', axis = 1, inplace = True)
+    df_MR = pd.melt(df_R, id_vars = 'Date', value_vars = list(df_R.columns).remove('Date'),
+                var_name = 'State', value_name = 'Recovered')
     df_MR['Recovered'] = pd.to_numeric(df_MR['Recovered'])
-    df_MR.drop('date', axis = 1, inplace = True)
-    df_MR = df_MR.groupby('state').sum()
+    df_MR.drop('Date', axis = 1, inplace = True)
+    df_MR = df_MR.groupby('State').sum()
     df_MR = df_MR['Recovered']
     ##################################################################
     ########## ALL States Deceased Data ##############################
-    df_D = df_states[df_states['status'] == 'Deceased'].copy(deep = True)
-    df_D.drop('status', axis = 1, inplace = True)
-    df_MD = pd.melt(df_D, id_vars = 'date', value_vars = list(df_D.columns).remove('date'),
-                var_name = 'state', value_name = 'Deceased')
+    df_D = df_states[df_states['Status'] == 'Deceased'].copy(deep = True)
+    df_D.drop('Status', axis = 1, inplace = True)
+    df_MD = pd.melt(df_D, id_vars = 'Date', value_vars = list(df_D.columns).remove('Date'),
+                var_name = 'State', value_name = 'Deceased')
     df_MD['Deceased'] = pd.to_numeric(df_MD['Deceased'])
-    df_MD.drop('date', axis = 1, inplace = True)
-    df_MD = df_MD.groupby('state').sum()
+    df_MD.drop('Date', axis = 1, inplace = True)
+    df_MD = df_MD.groupby('State').sum()
     df_MD = df_MD['Deceased']
     ##################################################################
     return df_MC, df_MR, df_MD
@@ -259,31 +265,31 @@ def update_state_name(state):
     return state_dict[state]
 
 def getForStatus(data, status_value):
-    df = data[data['status'] == status_value].copy(deep = True)
-    df.drop('status', axis = 1, inplace = True)
-    df.drop('tt', axis = 1, inplace = True)
-    df.drop('un', axis = 1, inplace = True)
-    temp_date = df['dateymd']
-    df.drop('dateymd', axis = 1, inplace = True)
+    df = data[data['Status'] == status_value].copy(deep = True)
+    df.drop('Status', axis = 1, inplace = True)
+    df.drop('TT', axis = 1, inplace = True)
+    df.drop('UN', axis = 1, inplace = True)
+    temp_date = df['Date_YMD']
+    df.drop('Date_YMD', axis = 1, inplace = True)
     df = df.apply(pd.to_numeric)
-    df['dateymd'] = pd.to_datetime(temp_date)
+    df['Date_YMD'] = pd.to_datetime(temp_date)
 
-    df = pd.melt(df, id_vars = 'dateymd',
-                         value_vars = list(df.columns).remove('dateymd'),
-                         var_name = 'state', value_name = status_value)
-    df = df.groupby('state').sum()
+    df = pd.melt(df, id_vars = 'Date_YMD',
+                         value_vars = list(df.columns).remove('Date_YMD'),
+                         var_name = 'State', value_name = status_value)
+    df = df.groupby('State').sum()
 
-    df['state_name'] = [update_state_name(x) for x in df.index]
+    df['State_name'] = [update_state_name(x) for x in df.index]
     return df
 
 def return_arr(df, status):
     arr = []
     for x, y in df.iterrows():
-        arr.append(y['state_name'] + " (" + str(y[status]) + ")")
+        arr.append(y['State_name'] + " (" + str(y[status]) + ")")
     return arr
 
 def generate_table(data):
-    data.drop('date', axis = 1, inplace = True)
+    data.drop('Date', axis = 1, inplace = True)
     today_data = data.tail(3)
     data = today_data.copy(deep = True)
     today_confirmed = getForStatus(data, "Confirmed")
@@ -342,14 +348,15 @@ def percentage_info(data):
 
 
 def start():
-    urllib.request.urlretrieve(url, 'data.json');
+    urllib.request.urlretrieve(url, 'state_wise_daily.csv');
+    # urllib.request.urlretrieve(url, 'state_wise_daily.csv');
     df = make_data()
 
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
     STATIC_DIR = os.path.join(BASE_DIR, 'static')
     TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-    create_pie_chart(df, STATIC_DIR, TEMPLATE_DIR)
+    #create_pie_chart(df, STATIC_DIR, TEMPLATE_DIR)
     create_line_chart(df, STATIC_DIR, TEMPLATE_DIR)
     df_bar = create_dataset(df)
     df_MC, df_MR, df_MD = overall_info(df)
